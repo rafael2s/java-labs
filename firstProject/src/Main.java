5104aed5d912201f9df4fc596824acb097bd1828
Rafael Souza <rafael_popo@hotmail.com>
2025-11-11 15:37:16 -0300

    construindo catalago de filmes

--- firstProject/src/Main.java
+++ firstProject/src/Main.java
@@ -2,14 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é Screen Match");
+        System.out.println("Filme: Tup Gun: Maverick");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int releaseYear = 2022;
+        System.out.println("Ano de lançamento: " + releaseYear);
+        boolean isIncludeInPlan = true;
+        double movieRating = 8.1;
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + releaseYear;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
     }
 }
