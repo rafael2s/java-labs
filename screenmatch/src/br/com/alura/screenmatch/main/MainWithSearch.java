459eabcc6df0215475e3ec7c32a2a7dc993e9cc5
Rafael Souza <rafael_popo@hotmail.com>
2025-11-17 20:33:24 -0300

    Utilizando lib externa

--- screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.models.Title;
+import br.com.alura.screenmatch.models.TitleOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,6 +27,14 @@ public class MainWithSearch {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
         System.out.println(response.body());
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        TitleOmdb myTitleOmdb =  gson.fromJson(json, TitleOmdb.class);
+        System.out.println(myTitleOmdb);
+        Title myTitle = new Title(myTitleOmdb);
+        System.out.println("Titulo ja convertido");
+        System.out.println(myTitle);
     }
 }
--- screenmatch/src/br/com/alura/screenmatch/models/Title.java
+++ screenmatch/src/br/com/alura/screenmatch/models/Title.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Title implements Comparable<Title> {
+    @SerializedName("Title")
     private String name;
+    @SerializedName("Year")
     private int releaseYear;
     private boolean isIncludeInPlan;
     private double sumRating;
@@ -11,6 +15,12 @@ public class Title implements Comparable<Title> {
     public Title(String name, int releaseYear) {
         this.name = name;
         this.releaseYear = releaseYear;
+    }
+
+    public Title(TitleOmdb myTitleOmdb) {
+        this.name = myTitleOmdb.title();
+        this.releaseYear = Integer.valueOf(myTitleOmdb.year());
+        this.durationInMinutes = Integer.valueOf(myTitleOmdb.runtime().substring(0,2));
     }
 
     public String getName() {
@@ -66,5 +76,12 @@ public class Title implements Comparable<Title> {
     @Override
     public int compareTo(Title anotherTitle) {
         return this.getName().compareTo(anotherTitle.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", releaseYear=" + releaseYear + "," +
+                " duration=" + durationInMinutes;
     }
 }
