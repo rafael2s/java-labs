7a5d2e437b42c63ad9380e0de7a4d7b3c1e65212
Rafael Souza <rafael_popo@hotmail.com>
2025-11-17 19:51:26 -0300

    Entendendo API

---
+++ screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner reading = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var search = reading.nextLine();
+
+        String adress = "https://www.omdbapi.com/?t=" + search + "&apikey=5043abc8";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(adress))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
