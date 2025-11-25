405342ff0c2cc4d4ca2a46717529539687cadcdd
Rafael Souza <rafael_popo@hotmail.com>
2025-11-25 06:33:50 -0300

    Modelando dados da aplicação

--- screenmatchsb/src/main/java/br/com/rafael/screematchsb/ScreematchsbApplication.java
+++ screenmatchsb/src/main/java/br/com/rafael/screematchsb/ScreematchsbApplication.java
@@ -1,8 +1,6 @@
 package br.com.rafael.screematchsb;
 
-import br.com.rafael.screematchsb.model.DadosSerie;
-import br.com.rafael.screematchsb.service.ConsumoApi;
-import br.com.rafael.screematchsb.service.ConverteDados;
+import br.com.rafael.screematchsb.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class ScreematchsbApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=5043abc8");
-        //System.out.println(json);
-        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
     }
 }
---
+++ screenmatchsb/src/main/java/br/com/rafael/screematchsb/principal/Principal.java
@@ -0,0 +1,47 @@
+package br.com.rafael.screematchsb.principal;
+
+import br.com.rafael.screematchsb.model.DadosEpisodio;
+import br.com.rafael.screematchsb.model.DadosSerie;
+import br.com.rafael.screematchsb.model.DadosTemporada;
+import br.com.rafael.screematchsb.service.ConsumoApi;
+import br.com.rafael.screematchsb.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=5043abc8";
+
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para buscar");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<= dados.totalTemporadas(); i++ ){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach((System.out::println));
+
+//        for(int i = 0;  i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
+}
